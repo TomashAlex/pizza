34eab609c9380e9af34592cf5aa1cc5d93b7b002
Aleksandr Tomashevskiy <tomashevskiy.aleksandr@simpleone.ru>
2022-01-25 22:15:40 +0300

    package

--- js/pizzaType.js
+++ js/pizzaType.js
@@ -1,4 +1,16 @@
+import {pizzaPrice as $} from "../pizzaPrice";
 
+/**
+ * Увеличение цена
+ * Получает цену пиццы в зависимости от размера
+ */
+ function getPizzaPrice (size, element) {
+   let nodeElement = element.parentNode
+   let mainParent = nodeElement.parentElement.parentElement.parentElement
+   let pizzaName = mainParent.querySelector('.item__title span')
+   let pizzaPrice = mainParent.querySelector('.pizza-block_price span')
+   pizzaPrice.textContent = $[size][pizzaName];
+}
 
 /**
  * Измененеие цвета фильтрации у элементов 
@@ -18,8 +30,6 @@ for (const filterElement of filterElements) {
 }
 
 
-
-
 /**
  * Переключение между типами теста
  */
@@ -27,6 +37,7 @@ let pizzaTypeElements = document.querySelectorAll('.pizza_type li');
 
 for (let i = 0; i < pizzaTypeElements.length; i++){
    pizzaTypeElements[i].onclick = function () {
+   
       if (pizzaTypeElements[i].textContent == 'традиционное') {
          pizzaTypeElements[i-1].classList.remove('active');
          pizzaTypeElements[i].classList.add('active');
@@ -41,27 +52,28 @@ for (let i = 0; i < pizzaTypeElements.length; i++){
  * Переключение между размером
  */
 let pizzaSizeElements = document.querySelectorAll('.pizza_size li');
+
 for (let i = 0; i < pizzaSizeElements.length; i++){
    pizzaSizeElements[i].onclick = function () {
+   
+
     if (pizzaSizeElements[i].textContent == '26 см'){
       pizzaSizeElements[i].classList.add('active');
       pizzaSizeElements[i+1].classList.remove('active');
       pizzaSizeElements[i+2].classList.remove('active');
+      getPizzaPrice(pizzaSizeElements[i].textContent, pizzaSizeElements[i]);
+      
      } else if (pizzaSizeElements[i].textContent == '30 см'){
       pizzaSizeElements[i].classList.add('active');
       pizzaSizeElements[i-1].classList.remove('active');
       pizzaSizeElements[i+1].classList.remove('active');
-      
-     } else {
+        getPizzaPrice(pizzaSizeElements[i].textContent ,pizzaSizeElements[i]);
+     
+   } else {
       pizzaSizeElements[i].classList.add('active');
       pizzaSizeElements[i-1].classList.remove('active');
       pizzaSizeElements[i-2].classList.remove('active');
-     }
+      getPizzaPrice(pizzaSizeElements[i].textContent ,pizzaSizeElements[i]);
+   }
    }
 }
-/**
- * Цена
- */
-
-
-
